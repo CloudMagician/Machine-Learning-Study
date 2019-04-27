52ca81463b005e1ebcada2161f60ef521a809297
CloudMagician <luzixv1998@icloud.com>
2019-04-27 20:46:43 +0800

    ...

--- Logistic Regression/Horse-colic/logRegres.py
+++ Logistic Regression/Horse-colic/logRegres.py
@@ -1,18 +1,21 @@
+# -*- coding: UTF-8 -*-
+
 import numpy as np
+import array
 import random
 
 def sigmoid(inX):
     return 1.0 / (1 + np.exp(-inX))
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
-    m,n = np.shape(dataMatrix)                                                #返回dataMatrix的大小。m为行数,n为列数。
-    weights = np.ones(n)                                                       #参数初始化                                        #存储每次更新的回归系数
+    m,n = np.shape(dataMatrix)                   #返回dataMatrix的大小。m为行数,n为列数。
+    weights = np.ones(n)                           #参数初始化#存储每次更新的回归系数
     for j in range(numIter):                                           
         dataIndex = list(range(m))
         for i in range(m):           
-            alpha = 4/(1.0+j+i)+0.01                                            #降低alpha的大小，每次减小1/(j+i)。
-            randIndex = int(random.uniform(0,len(dataIndex)))                #随机选取样本
-            h = sigmoid(sum(dataMatrix[randIndex]*weights))                    #选择随机选取的一个样本，计算h
+            alpha = 4/(1.0+j+i)+0.01   #降低alpha的大小，每次减小1/(j+i)。
+            randIndex = int(random.uniform(0,len(dataIndex)))   #随机选取样本
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))   #选择随机选取的一个样本，计算h
             error = classLabels[randIndex] - h                                 #计算误差
             weights = weights + alpha * error * dataMatrix[randIndex]       #更新回归系数
             del(dataIndex[randIndex])                                         #删除已经使用的样本
@@ -24,14 +27,14 @@ def classifyVector(inX, weights):
     else: return 0.0
 
 def colicTest():
-    frTrain = open('horse-colic.data')
-    frTest = open('horse-colic.test')
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
     trainingSet = []
     trainingLabels = []
     for line in frTrain.readlines():
         currLine = line.strip().split('\t') 
         lineArr = []
-        for i in range(21): lineArr.append(float(currLine[i])
+        for i in range(21): lineArr.append(float(currLine[i]))
         trainingSet.append(lineArr)
         trainingLabels.append(float(currLine[21]))
         trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
@@ -43,13 +46,13 @@ def colicTest():
             lineArr = []
             for i in range(21): lineArr.append(float(currLine[i]))
             if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
-            errorCount += 1
+                errorCount += 1
         errorRate = (float(errorCount)/numTestVec)
-        print "the error rate of this test is: %f" % errorRate
+        print("the error rate of this test is: %f" % errorRate)
         return errorRate
 
 def multiTest ():
     numTests = 10
     errorSum = 0.0
     for k in range(numTests): errorSum += colicTest()
-    print "after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests))
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
